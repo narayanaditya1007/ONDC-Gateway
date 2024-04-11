3091f1640dd3efaacbf8654e0cfcdbb5a9c00457
Aditya Narayan <narayanaditya1007@gmail.com>
2024-04-11 18:44:29 +0530

    remove email redundancy

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,6 +9,12 @@ require('dotenv').config();
 async function signup(req,res){
     try{
         console.log("hello");
+        const existUser = await User.findOne({email:req.body.email});
+        if(existUser){
+            console.log("user exists");
+            res.send("Email already exists, Try loggin in")
+            throw new Error("Email already exists, Try loggin in")
+        }
         const hashPass = await bcrypt.hash(req.body.password,10);
         const myId = new mongoose.Types.ObjectId();
         const user = new User({
